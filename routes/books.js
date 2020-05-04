bda91d1e26ae6ade0ad6071fc881c461702546d0
Caroline Evangelista <caroline.evangelista@melhorenvio.com>
2020-05-04 17:01:12 -0300

    CRUD books

--- routes/books.js
+++ routes/books.js
@@ -3,22 +3,79 @@ var booksModel = require("../models/bookModel");
 
 router.get('/', (req, res) => {    
     //Crie aqui a função para listar todos os livros cadastrados na estrutura (models/bookModel) do mongodb 
-    // res.json({
-    //     success: true,
-    //     books: []
-    // })
-})
+    booksModel.find(function (err, books) {
+        if  (err)
+            return res.send(err);
 
+        res.json(books);
+        // res.json({
+        //     success: true,
+        //     books: ""
+        // }, [ books])
+    })
+})
+/**
+ * Crie aqui a função para atualizar os dados de um livro com base no id do mesmo
+ */
 router.put('/:id', (req, res) => {
-    //Crie aqui a função para atualizar os dados de um livro com base no id do mesmo 
+    booksModel.findOne({
+        _id: req.params.id
+    })
+        .then(book => {
+            allowedParams = [
+                "title",
+                "category",
+                "pageCount",
+                "publishedDate",
+                "thumbnailUrl",
+                "shortDescription",
+                "longDescription"
+            ];
+
+            for (param in req.body) {
+                if (allowedParams.includes(param))
+                    book[param] = req.body[param]
+            }
+
+            book.save(function (err) {
+                if (err)
+                    res.send(err)
+
+                res.json({ message: 'book updated!' });
+            })
+        })
+        .catch((err) => {
+            return res.json(err)
+        })
 })
 
+/**
+ * Crie aqui a função para cadastrar os dados de um livro
+ */
 router.post('/', (req, res) => {
-     //Crie aqui a função para cadastrar os dados de um livro  
+    const book = new booksModel(req.body)
+
+    book.save()
+        .then(book => {
+            return res.json(book)
+        })
+        .catch(err => {
+            res.status(500).json({msg: 'Sorry, internal server errors'})
+            console.log(err)
+            return;
+        })
 })
 
-router.delete('/', (req, res) => {
-    //Crie aqui a função para remover os dados de um livro  
+router.delete('/delete/:id', (req, res) => {
+    //Crie aqui a função para remover os dados de um livro
+    booksModel.deleteOne({
+        _id: req.params.id
+    }, function(error) {
+        if(error)
+            res.send(error);
+
+        res.json({ message: 'Usuário excluído com Sucesso! '});
+    });
 })
 
 module.exports = router;
